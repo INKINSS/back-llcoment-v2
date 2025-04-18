3cb9a248f0466e5ca08c50712b62433a9b8b75f5
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-04-18 15:56:10 -0600

    fix: corrects the required data sent from the front

--- src/schemas/article.schema.js
+++ src/schemas/article.schema.js
@@ -1,12 +1,11 @@
-import { z } from 'zod'
+import { z } from "zod";
 
 const ArticleSchema = z.object({
-    title: z.string(),
-    content: z.string(),
-    tags: z.array(z.string()),
-    author: z.string().default('anónimo'),
-    createAt: z.date().default(new Date()),
-    updateAt: z.date().default(new Date())
-})
+  content: z.string(),
+  tag: z.string(),
+  nickname: z.string().default("anónimo"),
+  createAt: z.date().default(new Date()),
+  updateAt: z.date().default(new Date()),
+});
 
-export default ArticleSchema
+export default ArticleSchema;
