84bef76519834c00816ad6702583379d6aea2c74
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-04-18 15:54:10 -0600

    fix(main files): fixes cors error and receiving data in JSON format

--- src/app.js
+++ src/app.js
@@ -1,16 +1,17 @@
 import express from "express";
 import router from "./routes/articles.routes.js";
 import conectDB from './config/db.js'
+import cors from 'cors'
 const app = express();
 
 const PORT = process.env.PORT || 3000;
 
 conectDB()
 
-app.use("/", router);
-
-app.use(express.json());
+app.use(cors())
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/", router);
 
 
 export default app;
